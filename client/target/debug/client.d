95eb96cc9ef97fac39b8f144bc38a3785e50faef
Lenny Boyatzis <lennyboyatzis@gmail.com>
2018-02-11 15:59:00 +1100

    chat app in rust

---
+++ client/target/debug/client.d
@@ -0,0 +1,1 @@
+/Users/lenny.boyatzis/Lenny/rust/learning/chat/client/target/debug/client.dSYM: /Users/lenny.boyatzis/Lenny/rust/learning/chat/client/src/main.rs
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/Users/lenny.boyatzis/Lenny/rust/learning/chat/server/target/debug/server.dSYM: /Users/lenny.boyatzis/Lenny/rust/learning/chat/server/src/main.rs
